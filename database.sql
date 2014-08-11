af3972aac3e2a880146698b0b4b424772fd3fbda
tupvse02404 <tupvse02404@fpt.edu.vn>
2014-08-11 16:13:25 +0700

    update edit category

--- database.sql
+++ database.sql
@@ -4,7 +4,7 @@ USE `auction`;
 --
 -- Host: 127.0.0.1    Database: auction
 -- ------------------------------------------------------
--- Server version	5.6.16-log
+-- Server version	5.6.19
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -30,12 +30,19 @@ CREATE TABLE `auction` (
   `seller_id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `description` text NOT NULL,
-  `start_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `end_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `start_date` date NOT NULL DEFAULT '0000-00-00',
+  `start_time` time NOT NULL,
+  `end_date` date NOT NULL DEFAULT '0000-00-00',
+  `end_time` time NOT NULL,
   `starting_price` double NOT NULL,
-  `reserve_price` double NOT NULL,
   `buy_now_price` double NOT NULL,
-  `status` varchar(20) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `image1` text,
+  `video` text,
+  `image2` text,
+  `image3` text,
+  `image4` text,
+  `image5` text,
   PRIMARY KEY (`auctionid`),
   KEY `category_id` (`category_id`),
   KEY `seller_id` (`seller_id`),
@@ -43,7 +50,7 @@ CREATE TABLE `auction` (
   FULLTEXT KEY `title_2` (`title`),
   CONSTRAINT `auction_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`categoryid`),
   CONSTRAINT `auction_ibfk_2` FOREIGN KEY (`seller_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -52,7 +59,6 @@ CREATE TABLE `auction` (
 
 LOCK TABLES `auction` WRITE;
 /*!40000 ALTER TABLE `auction` DISABLE KEYS */;
-INSERT INTO `auction` VALUES (1,1,1,'Samsung Focus','New and sleeky smartphone','2014-08-03 17:00:00','2014-08-20 17:00:00',56,70,100,'Active');
 /*!40000 ALTER TABLE `auction` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,35 +89,6 @@ INSERT INTO `category` VALUES (1,'Smartphone','Smartphone for the smart.'),(2,'R
 UNLOCK TABLES;
 
 --
--- Table structure for table `digital`
---
-
-DROP TABLE IF EXISTS `digital`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `digital` (
-  `auction_id` int(11) NOT NULL,
-  `image1` text,
-  `image2` text,
-  `image3` text,
-  `image4` text,
-  `image5` text,
-  `video` text,
-  PRIMARY KEY (`auction_id`),
-  CONSTRAINT `digital_ibfk_1` FOREIGN KEY (`auction_id`) REFERENCES `auction` (`auctionid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `digital`
---
-
-LOCK TABLES `digital` WRITE;
-/*!40000 ALTER TABLE `digital` DISABLE KEYS */;
-/*!40000 ALTER TABLE `digital` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `user`
 --
 
@@ -125,9 +102,9 @@ CREATE TABLE `user` (
   `password` varchar(20) NOT NULL,
   `phonenumber` varchar(12) DEFAULT NULL,
   `email` varchar(45) NOT NULL,
-  `address` varchar(45) DEFAULT NULL,
-  `role` enum('Admin','Staff','Customer') DEFAULT 'Customer',
-  `status` enum('Active','Inactive') DEFAULT 'Inactive',
+  `address` text,
+  `role` tinyint(1) DEFAULT '0',
+  `status` tinyint(1) DEFAULT '0',
   `salt` varchar(10) DEFAULT NULL,
   `balance` double DEFAULT '0',
   PRIMARY KEY (`id`),
@@ -142,7 +119,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Admin','admin','admin',NULL,'admin@admin.admin',NULL,'Admin','Active','121313121',0),(6,'Pham Van Tu','phamvantu','123','0987656323','tupvse02404@fpt.vn','Hai Duong','Customer','Inactive','2122922761',0),(7,'Staff','staff','staff','','ducchu@live.com','','Staff','Active','1960531220',0),(8,'Customer','customer','customer','','ducchu@live.com','','Customer','Active','1440368330',0);
+INSERT INTO `user` VALUES (1,'Admin','admin','admin',NULL,'admin@admin.admin',NULL,1,1,'121313121',0),(6,'Pham Van Tu','phamvantu','123','0987656323','tupvse02404@fpt.vn','Hai Duong',3,2,'2122922761',0),(7,'Staff','staff','staff','','ducchu@live.com','',2,1,'1960531220',0),(8,'Customer','customer','customer','','ducchu@live.com','',3,1,'1440368330',0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -155,4 +132,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-08-08 13:13:07
+-- Dump completed on 2014-08-11 16:12:41
