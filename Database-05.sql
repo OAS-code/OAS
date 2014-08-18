df7fec52f75fde58aace1b7a1d75d6b6597bb2d8
Maxime <ducchu@live.com>
2014-08-19 00:54:06 +0700

    Code Auction.

--- Database-05.sql
+++ Database-05.sql
@@ -43,6 +43,7 @@ CREATE TABLE `auction` (
   `img_4` text,
   `img_5` text,
   `v_youtube` text,
+  `views` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`auctionid`),
   KEY `category_id` (`category_id`),
   KEY `seller_id` (`seller_id`),
@@ -57,7 +58,7 @@ CREATE TABLE `auction` (
 
 LOCK TABLES `auction` WRITE;
 /*!40000 ALTER TABLE `auction` DISABLE KEYS */;
-INSERT INTO `auction` VALUES (1,9,43,'ASUS Zenfone 5','','2014-08-18 01:25:45','2014-09-25 01:15:00',50,250,5,0,'http://fptshop.com.vn/Uploads/images/asus-zenfone-5-4.png','http://cache.media.techz.vn/upload/2014/04/23/image-1398248544-Zenfone5_07.jpg',NULL,NULL,NULL,NULL,'https://www.youtube.com/watch?v=ixhPTFdwHAM'),(2,9,43,'Iphone 5S','','2014-08-18 01:47:46','2014-08-27 01:31:00',20,500,10,0,'http://cdn2.sbnation.com/assets/3256545/IMG_2909-1024.jpg',NULL,NULL,NULL,NULL,NULL,''),(3,10,43,'Honda Airblade 2010','','2014-08-18 04:25:57','2014-08-27 04:25:00',1200,5000,100,0,'http://dantri.vcmedia.vn/Uploaded/2010/05/16/3.jpg','http://1.bp.blogspot.com/_7G-rNqb-F4E/SqoAx1c9GyI/AAAAAAAAAZw/7Fe0wHvOhWw/s400/1209004801_Airblade_White.jpg','https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSoqBPQlSrkisWOYBm9pY5jFv1Mzws18PiBxfuIyqKqx-LjAl_X','http://farm10.gox.vn/tinmoi/store/images/thumb/22122012/6/1083387/can_anh_honda_air_blade_125_do_xang_khong_can_xuong_xe_1.jpg','','','https://www.youtube.com/watch?v=RP8XeWc-0h8'),(4,10,43,'Mercedes C200 2011','','2014-08-18 04:29:12','2014-08-30 04:28:00',15000,35000,300,0,'http://www.thuexedulich.org/files/trangchu/mercedes_c200.8.jpg',NULL,NULL,NULL,NULL,NULL,'https://www.youtube.com/watch?v=qTc7OtSWr-M');
+INSERT INTO `auction` VALUES (1,9,43,'ASUS Zenfone 5','','2014-08-18 15:27:18','2014-09-25 01:15:00',50,250,5,0,'http://fptshop.com.vn/Uploads/images/asus-zenfone-5-4.png','http://cache.media.techz.vn/upload/2014/04/23/image-1398248544-Zenfone5_07.jpg',NULL,NULL,NULL,NULL,'https://www.youtube.com/watch?v=ixhPTFdwHAM',1),(2,9,43,'Iphone 5S','','2014-08-18 23:54:25','2014-08-27 01:31:00',20,500,10,0,'http://cdn2.sbnation.com/assets/3256545/IMG_2909-1024.jpg',NULL,NULL,NULL,NULL,NULL,NULL,2),(3,10,43,'Honda Airblade 2010','','2014-08-18 23:54:25','2014-08-27 04:25:00',1200,5000,100,0,'http://dantri.vcmedia.vn/Uploaded/2010/05/16/3.jpg','http://1.bp.blogspot.com/_7G-rNqb-F4E/SqoAx1c9GyI/AAAAAAAAAZw/7Fe0wHvOhWw/s400/1209004801_Airblade_White.jpg','https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSoqBPQlSrkisWOYBm9pY5jFv1Mzws18PiBxfuIyqKqx-LjAl_X','http://farm10.gox.vn/tinmoi/store/images/thumb/22122012/6/1083387/can_anh_honda_air_blade_125_do_xang_khong_can_xuong_xe_1.jpg',NULL,NULL,'https://www.youtube.com/watch?v=RP8XeWc-0h8',10),(4,10,43,'Mercedes C200 2011','','2014-08-18 15:27:18','2014-08-30 04:28:00',15000,35000,300,0,'http://www.thuexedulich.org/files/trangchu/mercedes_c200.8.jpg',NULL,NULL,NULL,NULL,NULL,'https://www.youtube.com/watch?v=qTc7OtSWr-M',5);
 /*!40000 ALTER TABLE `auction` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -140,7 +141,7 @@ CREATE TABLE `user` (
   UNIQUE KEY `username` (`username`,`email`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   FULLTEXT KEY `fullname` (`fullname`,`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -149,7 +150,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (8,'customer','807908e8af11eb3f416ae163762d4739','duccn01663@gmail.com','Customer1','1','1',0,0,'1823378392',0,'2014-08-13 17:01:22'),(14,'admin','807908e8af11eb3f416ae163762d4739','ducchu@live.com','Chu Nhu Duc1','097.847.9643','Hanoi HN',2,1,'1823378392',0,'2014-08-13 17:01:22'),(43,'staff','1393975c84ff83deba3f7050b910cf68','duccn01663@fpt.edu.vn','staff','','',1,1,'1168119770',0,'2014-08-14 13:17:39'),(44,'admin22','2d6ab00624c6257f7e57d0ec9bbbfed3','asdasdasd@asdasd.com','asdasdasd','asdasdasd','asdasdasd',1,0,'1862159749',0,'2014-08-15 10:20:37'),(45,'111111111111111','166dc49d526214932c8b780eaa92125c','22222222222222222222@.','3333','55555','44444',0,1,'1208191374',0,'2014-08-15 13:35:12');
+INSERT INTO `user` VALUES (8,'customer','807908e8af11eb3f416ae163762d4739','duccn01663@gmail.com','Customer1','1','1',0,0,'1823378392',0,'2014-08-13 17:01:22'),(14,'admin','807908e8af11eb3f416ae163762d4739','ducchu@live.com','Chu Nhu Duc1','097.847.9643','Hanoi HN',2,1,'1823378392',0,'2014-08-13 17:01:22'),(43,'staff','1393975c84ff83deba3f7050b910cf68','duccn01663@fpt.edu.vn','staff','','',1,1,'1168119770',0,'2014-08-14 13:17:39'),(44,'admin22','2d6ab00624c6257f7e57d0ec9bbbfed3','asdasdasd@asdasd.com','asdasdasd','asdasdasd','asdasdasd',1,0,'1862159749',0,'2014-08-15 10:20:37'),(45,'111111111111111','166dc49d526214932c8b780eaa92125c','22222222222222222222@.','3333','55555','44444',0,1,'1208191374',0,'2014-08-15 13:35:12'),(46,'','93b54aa30e37bdcbc1787d3e9bc9bd9c','','','','',0,1,'1611501300',0,'2014-08-18 14:52:18'),(47,'sdsdasdadasd','fb560ead98d96d3bb7dcb02e817bf5f4','asdasdasd','','','',0,1,'1573971205',0,'2014-08-18 14:52:36'),(50,'1111111111111','bb0dbe4e19beeab896f0e3b0f000f202','111111111','','','',0,1,'1842228419',0,'2014-08-19 00:35:43'),(53,'11111111','8848befacf0a2a94fbf7a26818cd9419','sdsd','','','',0,1,'1520336637',0,'2014-08-19 00:38:43'),(60,'111111112','5290b4a58898b4e925a68ce96a15c2b3','sdsd@.','Addasdasd','vsss','asdsd',0,1,'1950023030',0,'2014-08-19 00:39:42'),(79,'ssdsd','e836e502416a3d4e153e66f893374faf','asdasdsd@.sd','','','',0,1,'1200541485',0,'2014-08-19 00:48:44');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -162,4 +163,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-08-18  5:05:35
+-- Dump completed on 2014-08-19  0:54:00
