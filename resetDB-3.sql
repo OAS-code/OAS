d5e280b0122efec7965da963af2c0ae48302e59f
Maxime <ducchu@live.com>
2014-08-28 21:44:10 +0700

    code

--- resetDB-3.sql
+++ resetDB-3.sql
@@ -1,4 +1,4 @@
-update auction set moderate_status = 0 , seller_id=83, close_date=null, buyer_confirm='' ;
+update auction set moderate_status = 0 , seller_id=8, close_date=null, buyer_confirm='' ;
 update user set balance = 15000;
 delete from bid;
 delete from transaction;
